7bb10c8355d095333846165a543347f93839dedb
Nikhil Kumar Reddy <mailtonikhilkumarreddy@gmail.com>
2021-09-24 22:11:03 +0530

    error in while appending the dataframe

---
+++ page_1.py
@@ -0,0 +1,111 @@
+from bs4 import BeautifulSoup
+import requests
+
+class page_01:
+    def scrap_1(self,link): #doctor page link ..
+        a = []
+        html_text_1 = requests.get(link).text
+        soup_1 = BeautifulSoup(html_text_1, 'lxml')
+        table_1 = soup_1.find('table', class_='table-bordered table-striped table-condensed cf')
+        inner_table_1 = table_1.find('tbody')
+        details_1 = inner_table_1.find_all('tr')
+        for each in details_1:
+            try:
+                each_ind = each.find_all('td')
+                for each_ind_details in each_ind:
+                    continue
+
+                link_11 = each_ind_details.a['href']
+                a.append("https://www.drdata.in/" + link_11)
+            except:
+                pass
+
+            #print("https://www.drdata.in/" + link_11)
+            #linkss = "https://www.drdata.in/" + link_11
+        return a
+
+    def scrap_2(self,link): #next pages link
+        ac = []
+        html_text_1 = requests.get(link).text
+        soup_2 = BeautifulSoup(html_text_1, 'lxml')
+        #next_ = soup_2.find('div',class_= 'col-sm-12')#
+        find_a =soup_2.find_all('a',class_='pnavi')
+        for i in find_a:
+            ac.append("https://www.drdata.in/"+i['href'].replace(" ",'%20'))
+        return len(ac) , ac
+        #print(len(find_a))
+
+    def doctor_details(self,link_): #extract the exact details what we want
+
+        actual_details = []
+        tail = ['Name', 'Specialization', 'Degree', 'Area of Practice', 'Date of Birth', 'Address', 'State', 'District', 'Geographical Area','Phone Number','Mobile Number and Email']
+        html_text_3 = requests.get(link_).text
+        soup_3 = BeautifulSoup(html_text_3, 'lxml')
+        find_table = soup_3.find('table',class_= 'table-bordered table-striped table-condensed cf')
+        try:
+            details = find_table.find_all('td')
+            for i in range(0,len(details)):
+                #print(details[i].text)
+                #header.append(details[i].text)
+                if details[i].text == 'Name':
+                    if details[i+1].text == '':
+                        actual_details.append('NaN')
+                    else:
+                        actual_details.append(details[i+1].text)
+
+                if details[i].text == 'Specialization':
+                    if details[i + 1].text == '':
+                        actual_details.append('NaN')
+                    else:
+                        actual_details.append(details[i+1].text)
+                if details[i].text == 'Degree':
+                    if details[i + 1].text == '':
+                        actual_details.append('NaN')
+                    else:
+                        actual_details.append(details[i+1].text)
+                if details[i].text == 'Area of Practice':
+                    if details[i + 1].text == '':
+                        actual_details.append('NaN')
+                    else:
+                        actual_details.append(details[i+1].text)
+                if details[i].text == 'Date of Birth':
+                    if details[i + 1].text == '':
+                        actual_details.append('NaN')
+                    else:
+                        actual_details.append(details[i+1].text)
+                if details[i].text == 'Address':
+                    if details[i + 1].text == '':
+                        actual_details.append('NaN')
+                    else:
+                        actual_details.append(details[i+1].text)
+                if details[i].text == 'State':
+                    if details[i + 1].text == '':
+                        actual_details.append('NaN')
+                    else:
+                        actual_details[i].append(details[i+1].text)
+                if details[i].text == 'District':
+                    if details[i + 1].text == '':
+                        actual_details.append('NaN')
+                    else:
+                        actual_details.append(details[i+1].text)
+                if details[i].text == 'Geographical Area':
+                    if details[i + 1].text == '':
+                        actual_details.append('NaN')
+                    else:
+                        actual_details.append(details[i+1].text)
+                if details[i].text == 'Phone Number':
+                    if details[i + 1].text == '':
+                        actual_details.append('NaN')
+                    else:
+                        actual_details.append(details[i+1].text)
+
+
+
+                #d[details[i].text] = details[i+1].text
+        except:
+            pass
+        return actual_details #returns the list of doctors from given page
+
+
+
+
---
+++ pp.py
@@ -0,0 +1,65 @@
+import time
+from bs4 import BeautifulSoup
+import requests
+from page_1 import page_01
+import numpy as np
+import pandas as pd
+
+head = ['Name', 'Specialization', 'Degree', 'Area of Practice', 'Date of Birth', 'Address', 'State', 'District',
+        'Geographical Area', 'Phone Number'] # details that we are scrapping
+
+df = pd.DataFrame(columns=head)
+html_text = requests.get('https://www.drdata.in/').text
+soup = BeautifulSoup(html_text, 'lxml')
+states = soup.find_all('div', class_= 'panel panel-default')
+doctors_links = []
+index_ = 0
+"""for state in states:
+    count_of_doctors_in_this_state = 0
+    a = state.find('div', class_='panel-collapse collapse').find('div', class_  = 'panel-body')
+    c = a.find('ul')
+    d = c.find('li')
+    k = d.a['href']
+    j = d.a.h4.text
+    print(j) #prints the state name for reference
+
+    link_1 = "https://www.drdata.in/"+k #0th  page
+
+    obj_1 = page_01()
+    link_1st_page = obj_1.scrap_1(link_1)
+    if len(link_1st_page) != 0 :
+        #print(link_1st_page) #gives the 1st page of doctors links
+        for ii in link_1st_page:
+            list_ = obj_1.doctor_details(ii)
+            #print(len(list_)," ",list_)
+            count_of_doctors_in_this_state += 1
+            if len(list_) == 10: #if our len of  requirements matches going to write into dataframe
+                df.loc[index_] = list_
+                index_ += 1
+                print(len(list_), " ", list_)
+        time.sleep(10)
+
+    #doctors_links.append(link_1st_page)
+    length_ , arr  = obj_1.scrap_2(link_1) # gives all the next pages of doctors of doctors link
+    for ij in arr:
+        list_all_next_pages = obj_1.scrap_1(ij)
+        if len(list_all_next_pages) != 0:
+            #print(list_all_next_pages)
+            #doctors_links.append(list_all_next_pages)
+            for i in list_all_next_pages:
+                list__ = obj_1.doctor_details(i)
+
+                count_of_doctors_in_this_state +=1
+                if len(list__) == 10:
+                    df.loc[index_] = list__
+                    index_ += 1
+                    print("next page" ,len(list__), " ", list__)
+        time.sleep(20)
+    print(df.size)
+    print(count_of_doctors_in_this_state , " ",j)
+
+df.to_csv('out_put.csv')
+print("DOne")"""
+
+
+print(df.head(5))
